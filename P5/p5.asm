c42eed646c8140a0860b4434c510793026bd8201
RamonMoraila <r2209470@uabc.edu.mx>
2026-09-25 01:14:06 +0000

    Cambios

--- P5/p5.asm
+++ P5/p5.asm
@@ -24,7 +24,7 @@ call pHex_dw
 mov al,10       ; cambio de linea
 call putchar
 
-push bx;
+push bx
 
 mov al,10       ; cambio de linea
 call putchar
@@ -42,8 +42,12 @@ mul bl
 
 call pHex_w
 
-mov al,10       ; cambio de linea
+push ax
+
+mov al, 10
 call putchar
+
+pop ax
 
 mov [n],al
 
@@ -62,6 +66,68 @@ call pHex_b
 
 mov al,10       ; cambio de linea
 call putchar
+
+mov ax, 0xFF
+
+push ax
+
+mov al,10       ; cambio de linea
+call putchar
+
+pop ax
+
+div bx
+
+call pHex_w
+
+mov al,10       ; cambio de linea
+call putchar
+
+mov ax,dx
+
+call pHex_w
+
+push ax
+
+mov al,10       ; cambio de linea
+call putchar
+
+pop ax
+
+add ax,[n]
+
+call pHex_w
+
+push ax
+
+mov al,10       ; cambio de linea
+call putchar
+
+pop ax
+
+mov [n],ax
+
+mov ax,[n]
+
+call pHex_w
+
+push ax
+
+mov al,10       ; cambio de linea
+call putchar
+
+pop ax
+
+dec byte[n]
+
+mov ax,[n]
+
+call pHex_w
+
+mov al,10       ; cambio de linea
+call putchar
+
+pop bx
 
 mov eax, 1	;system call number (sys_exit) -- fin del programa
 int 0x80        ;call kernel
